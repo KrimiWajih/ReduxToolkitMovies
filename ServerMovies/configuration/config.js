bf14d2c3583267fabd130f3107c9fbf2b5222919
KrimiWajih <krimi.wajih@gmail.com>
2025-04-03 17:36:00 +0100

    changed setting

--- ServerMovies/configuration/config.js
+++ ServerMovies/configuration/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 exports.config = async () => {
     try {
-        await mongoose.connect("mongodb+srv://Krimiwajih:Krimiwajih1990@softdev.ou3v7.mongodb.net/Movies_DB")
+        await mongoose.connect("mongodb+srv://user:pass@softdev.ou3v7.mongodb.net/Movies_DB")
         console.log("Connected to Database Movies_DB")
     } catch (error) {
         console.log("Failed to Connect to Database")
